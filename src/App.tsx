caadb5fd1c96dd1c5bb05237e5373ec093a65f59
Ben Russert <benrussert@gmail.com>
2019-01-20 09:31:56 -0700

    Add styles with css loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 
 import DataExplorer from "@nteract/data-explorer";
-import "./App.css";
+
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/select/lib/css/blueprint-select.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+import "./nteract.css";
 
 import iris from "./data/iris.json";
 
-iris.data = iris.data.filter(d => d.index <= 10);
-
 class App extends Component {
-  onMetaDataChange = (metadata: object) => {
+  onMetaDataChange = (metadata: { dx?: any }) => {
     if (metadata && metadata.dx) {
       console.log(metadata.dx);
     }
